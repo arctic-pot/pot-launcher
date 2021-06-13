250f34fa17578277ba94bd50dbe22d810c039747
TheColdPot <guohw979@outlook.com>
2021-06-13 22:01:09 +0800

    chore: add localization fonts

--- src/welcome/LangChooser.tsx
+++ src/welcome/LangChooser.tsx
@@ -25,6 +25,7 @@ export default function LangChooser(props: PropsWithOnChoose): React.ReactElemen
             onChoose();
           }
         }}
+        lang={props.code}
       >
         <div className="chooser-title">{props.title}</div>
       </button>
@@ -36,11 +37,11 @@ export default function LangChooser(props: PropsWithOnChoose): React.ReactElemen
       <h1>Which language are you preferred in?</h1>
       <br />
       <div id="welcome-lang-choose">
-        <LangChooser title="English-US" code="en-US" />
-        <LangChooser title="English-UK" code="en-UK" disabled />
         <LangChooser title="简体中文" code="zh-CN" />
-        <LangChooser title="繁體中文-台灣" code="zh-TW" disabled />
-        <LangChooser title="繁體中文-香港" code="zh-HK" disabled />
+        <LangChooser title="臺灣標準中文" code="zh-TW" disabled />
+        <LangChooser title="香港標準中文" code="zh-HK" disabled />
+        <LangChooser title="English (US)" code="en-US" />
+        <LangChooser title="English (UK)" code="en-UK" disabled />
         <LangChooser title="Deutsche" code="de" disabled />
         <LangChooser title="日本語" code="ja" disabled />
         <LangChooser title="한국어" code="ko" disabled />
@@ -49,6 +50,10 @@ export default function LangChooser(props: PropsWithOnChoose): React.ReactElemen
         <LangChooser title="português" code="pt" disabled />
         <LangChooser title="Español" code="es" disabled />
       </div>
+      <p lang="zh-hk">
+        <br />香港特別行政區和澳門特別行政區使用的漢字標準相同<br />
+        香港、澳門、臺灣均爲中國的特別行政區
+      </p>
     </div>
   );
 }
