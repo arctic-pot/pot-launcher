bd4f4cea7cf7103a27a433e2307ff6a96cda919e
TheColdPot <guohw979@outlook.com>
2021-06-13 09:03:59 +0800

    refactor: use mixins

--- src/components/TitleBar.tsx
+++ src/components/TitleBar.tsx
@@ -2,16 +2,18 @@ import React from 'react';
 import './TitleBar.scss';
 
 export default function TitleBar(): React.ReactElement {
-  // TODO: I LOVE ELECTRON BECAUSE IT CAN'T USE IPC RENDER. I AM TRULY LOVVVVVE IT???
   return (
     <div id="title-bar">
       <span id="title">Pot Minecraft Launcher</span>
       <div id="title-buttons">
         {/* eslint-disable-next-line @typescript-eslint/no-var-requires */}
-        <div className="title-button" id="minimize-button">
+        <div className="title-button" onClick={() => location.reload()} title="Reload">
+          <i className="bi-arrow-clockwise" />
+        </div>
+        <div className="title-button" id="minimize-button" title="Minimize">
           <i className="bi-dash" />
         </div>
-        <div className="title-button" onClick={() => window.close()}>
+        <div className="title-button" onClick={() => window.close()} title="Close">
           <i className="bi-x" />
         </div>
       </div>
