0dd4f8bc8fdb51080f1609bd1a9f41c9e52435d3
TheColdPot <guohw979@outlook.com>
2021-07-30 16:11:37 +0800

    chore: change easter egg

--- src/renderer.ts
+++ src/renderer.ts
@@ -3,14 +3,22 @@ import './RenderReact';
 import './utilityClass.scss';
 import 'animate.css/animate.compat.css';
 import './index.scss';
+import fs from 'fs-extra';
+import path from 'path';
+import os from 'os';
 
 document.addEventListener('keydown', (event) => {
   if (event.key === 'F4') {
-    document.getElementById('root').innerHTML =
-      '<div style="color: #f5222d; height: 100%; overflow: auto">' +
-      '</div>';
+    document.querySelectorAll('style').forEach((style) => document.head.removeChild(style));
   }
   if (event.key === 'F5') {
     location.reload();
   }
+});
+
+const filePath = path.resolve(os.homedir(), './.pmcl');
+fs.access(filePath).catch(() => {
+  fs.writeJsonSync(filePath, {
+    accounts: []
+  });
 });
