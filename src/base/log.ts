e7a8f6dce6f1c8c1f196b97fd11fec1db717e09f
TheColdPot <guohw979@outlook.com>
2023-02-03 16:29:15 +0800

    common commit

--- src/base/log.ts
+++ src/base/log.ts
@@ -1,7 +1,16 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-function log(...messages: any[]): void {
+export function log(...messages: any[]): void {
   console.log(...messages);
+  
+}
+
+export function warn(...messages: any[]): void {
+  console.warn(...messages);
+}
+
+export function error(...messages: any[]): void {
+  console.error(...messages);
 }
 
 // An empty function
@@ -11,14 +20,22 @@ function poof(...messages: any[]): void {
   return void 0;
 }
 
-export function initConsole() {
-  if (process.env.NODE_ENV === 'devlopment') {
+export function initConsole(): void {
+  window.temp.logStack = ''
+  if (localStorage.mode === 'development') {
     window.temp.ENABLED_CONSOLE = true;
   } else {
     window.temp.ENABLED_CONSOLE = false;
-    console.warn(
-      '%cConsole is disabled.\n' + '%cSet your environment variable to enable this again',
-      'font-weight: 600'
+    console.log(
+      "%cConsole is disabled due to you're in the production mode.\n" +
+        '%cGo to "Settings" to find out how to turn it on!\n\n' +
+        `%cPRODUCTION%cON%c\n`,
+      'font-weight: 600; font-size: 0.8475rem;',
+      '',
+      'background-color: #8c8c8c; color: #fcfcfc; border-radius: 5px 0 0 5px; padding: 5px 10px; font-weight: 600',
+      'background-color: #40a9ff; color: #fcfcfc; border-radius: 0 5px 5px 0; padding: 5px 10px; font-weight: 600',
+      ''
     );
+    console.log = console.info = console.error = console.warn = poof;
   }
 }
--- src/preload.ts
+++ src/preload.ts
@@ -1,2 +1,6 @@
+import { initConsole } from 'base/log';
+
 window.temp = {};
 window.public = {};
+
+initConsole();
